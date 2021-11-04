2cf26d3ba439a03154a79d6c96563881af51fda9
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-11-04 01:36:47 -0300

    feat: adding producer and consumer

--- src/scripts/create-topic.sh
+++ src/scripts/create-topic.sh
@@ -1,4 +1,4 @@
-ocker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
+docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
     --create \
     --zookeeper zookeeper:2181 \
     --replication-factor 1 \
